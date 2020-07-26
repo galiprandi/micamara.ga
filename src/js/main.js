b753f73264c57b8eff5cb55298bd55699120f3db
Germán Aliprandi <galiprandi@gmail.com>
2020-07-26 20:05:57 -0300

    sw.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,6 @@ const app = new App({
 
 
 if ("serviceWorker" in navigator && location.hostname !== "192.168.1.50") {
-  console.log('ok');
   window.addEventListener("load", function () {
     navigator.serviceWorker.register("sw.js").then(
       function (registration) {
